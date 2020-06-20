93f5704a10a1bba79fb24546d719caf362d7c027
yarden <yardensch95@gmail.com>
2020-06-20 16:44:34 +0300

    fh

--- code/routes/recipes.js
+++ code/routes/recipes.js
@@ -114,11 +114,84 @@ router.get("/displayRecipePage/recipeId/:id", async (req, res) => {
     search_param.recipeId = req.params;
 
     //search the recipe
-    info_recipe = await search_functions.searchForSpecificRecipe(
-      search_param,
-      "full",
-      req
-    );
+    info_recipe = {
+      "id": 635342,
+      "image": "https://spoonacular.com/recipeImages/635342-556x370.jpg",
+      "title": "BLT Sandwich",
+      "readyInMinutes": 45,
+      "aggregateLikes": 63,
+      "vegetarian": false,
+      "vegan": false,
+      "glutenFree": false,
+      "ingredients": [
+          {
+              "name": "bell pepper",
+              "amount": 2,
+              "unit": "servings"
+          },
+          {
+              "name": "bread",
+              "amount": 4,
+              "unit": "pieces"
+          },
+          {
+              "name": "lettuce",
+              "amount": 2,
+              "unit": "servings"
+          },
+          {
+              "name": "mayonnaise",
+              "amount": 2,
+              "unit": "servings"
+          },
+          {
+              "name": "thick-cut bacon",
+              "amount": 8,
+              "unit": "pieces"
+          },
+          {
+              "name": "tomato",
+              "amount": 1,
+              "unit": ""
+          }
+      ],
+      "instructions": [
+          {
+              "number": 1,
+              "step": "Brown bacon in a skillet"
+          },
+          {
+              "number": 2,
+              "step": "Remove and pat off excess oil"
+          },
+          {
+              "number": 3,
+              "step": "Slice tomato into 1/4 inches slices"
+          },
+          {
+              "number": 4,
+              "step": "Toast bread"
+          },
+          {
+              "number": 5,
+              "step": "Spread a thin layer of mayonnaise on bread"
+          },
+          {
+              "number": 6,
+              "step": "Layer all ingredients on bread and close sandwich"
+          },
+          {
+              "number": 7,
+              "step": "Add fresh cracked black pepper"
+          }
+      ],
+      "servings": 2
+  };
+    // info_recipe = await search_functions.searchForSpecificRecipe(
+    //   search_param,
+    //   "full",
+    //   req
+    // );
     res.send(info_recipe);
     // search_functions
     //   .searchForSpecificRecipe(search_param, "full", req)
