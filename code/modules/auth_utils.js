fd528a59db4a1498828677b875b0f8d30e3b5211
eden <eden04060925@gmail.com>
2020-06-20 20:38:27 +0300

    oi

--- code/modules/auth_utils.js
+++ code/modules/auth_utils.js
@@ -3,44 +3,41 @@ var router = express.Router();
 const DButils = require("./DButils");
 const bcrypt = require("bcrypt");
 
-
-
-
-function registrationMissingParam(body){
-    if(!body.username){
-        return "missinng username input";
-      }
-      if(!body.password){
-       return "missinng password input";
-    }
-      if(!body.first_name){
-       return "missinng first name input";
-    }
-      if(!body.last_name){
-       return "missinng last name input";
-    }
-    if(!body.country){
-      return "missinng country input";
-    }
-    if(!body.email){
-      return "missinng email input";
-    }
-    if(!body.profile_pic){
-        return "missinng profile picture input";
-    }
-    return "";
+function registrationMissingParam(body) {
+  if (!body.username) {
+    return "missinng username input";
+  }
+  if (!body.password) {
+    return "missinng password input";
+  }
+  if (!body.first_name) {
+    return "missinng first name input";
+  }
+  if (!body.last_name) {
+    return "missinng last name input";
+  }
+  if (!body.country) {
+    return "missinng country input";
+  }
+  if (!body.email) {
+    return "missinng email input";
+  }
+  // if(!body.profile_pic){
+  //     return "missinng profile picture input";
+  // }
+  return "";
 }
 
-async function getUsersFronDB(){
-   users= await DButils.execQuery("SELECT username FROM users");
-   return users;
+async function getUsersFronDB() {
+  users = await DButils.execQuery("SELECT username FROM users");
+  return users;
 }
 
-async function addNewUser(req,users){
-    if (users.find((x) => x.username === req.body.username))
+async function addNewUser(req, users) {
+  if (users.find((x) => x.username === req.body.username))
     throw { status: 409, message: "Username already taken" };
 
-// encrypt password
+  // encrypt password
   let hash_password = bcrypt.hashSync(
     req.body.password,
     parseInt(process.env.bcrypt_saltRounds)
@@ -51,28 +48,26 @@ async function addNewUser(req,users){
   );
 }
 
-async function login(users,req){
-    if (!users.find((x) => x.username === req.body.username))
-        throw { status: 401, message: "Username or Password incorrect" };
+async function login(users, req) {
+  if (!users.find((x) => x.username === req.body.username))
+    throw { status: 401, message: "Username or Password incorrect" };
 
-    const user = (
-        await DButils.execQuery(
-       `SELECT * FROM users WHERE username = '${req.body.username}'`
+  const user = (
+    await DButils.execQuery(
+      `SELECT * FROM users WHERE username = '${req.body.username}'`
     )
   )[0];
-  let hash_password =await bcrypt.hashSync(
+  let hash_password = await bcrypt.hashSync(
     req.body.password,
     parseInt(process.env.bcrypt_saltRounds)
   );
-    if (!await bcrypt.compareSync(req.body.password, user.password)) {
-        throw { status: 401, message: "Username or Password incorrect" };
-    }
-    return user;
-       
+  if (!(await bcrypt.compareSync(req.body.password, user.password))) {
+    throw { status: 401, message: "Username or Password incorrect" };
+  }
+  return user;
 }
 
-
-exports.registrationMissingParam=registrationMissingParam;
-exports.getUsersFronDB=getUsersFronDB;
-exports.login=login;
-exports.addNewUser=addNewUser;
+exports.registrationMissingParam = registrationMissingParam;
+exports.getUsersFronDB = getUsersFronDB;
+exports.login = login;
+exports.addNewUser = addNewUser;
