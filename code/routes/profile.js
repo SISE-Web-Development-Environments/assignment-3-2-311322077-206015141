3a32543e38ef8d2f5b9ce82e98dee91a9cb715be
yarden <yardensch95@gmail.com>
2020-06-21 12:03:19 +0300

    ds

--- code/routes/profile.js
+++ code/routes/profile.js
@@ -31,23 +31,24 @@ router.use((req, res, next) => {
  */
 router.get("/recipeInfo/:ids", async (req, res, next) => {
   try {
-    // const recipes_ids_list = JSON.parse(req.params.ids);
-    // r = await search_functions.getRecipesPreviewInfo(recipes_ids_list);
-    // profile_utils
-    //   .getRecipeProfileInfo(req.user, recipes_ids_list)
-    //   .then((theResult) => {
-    //     res.send(theResult[0]);
-    //   })
-    console.log(req.params.ids);
-    a = {
-      "635342": {
-        watched: true,
-        saved: false,
-      },
-    };
-    res.send(a).catch((err) => {
-      throw { status: err.status, message: err.message };
-    });
+    const recipes_ids_list = JSON.parse(req.params.ids);
+    r = await search_functions.getRecipesPreviewInfo(recipes_ids_list);
+    profile_utils
+      .getRecipeProfileInfo(req.user, recipes_ids_list)
+      .then((theResult) => {
+        res.send(theResult[0]);
+      })
+      // console.log(req.params.ids);
+      // a = {
+      //   "635342": {
+      //     watched: true,
+      //     saved: false,
+      //   },
+      // };
+      // res.send(a).
+      .catch((err) => {
+        throw { status: err.status, message: err.message };
+      });
   } catch (err) {
     next(err);
   }
@@ -203,6 +204,7 @@ router.get("/familyRecipes/:ids", async (req, res, next) => {
     //   );
     // });
     // result = await Promise.all(promises);
+    // c= JSON.stringify(result);
     // res.send(JSON.stringify(result));
     a = [
       {
@@ -228,7 +230,7 @@ router.get("/familyRecipes/:ids", async (req, res, next) => {
         ],
         owner: "mother",
         where: "kidush",
-        pictures: ["pic1", "pic2"],
+        pictures: ["pic1"],
       },
     ];
     res.send(a);
